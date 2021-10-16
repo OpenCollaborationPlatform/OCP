c4caa291691f236f6fed30a633872cce5be7bbc7
Stefan Tröger <stefantroeger@gmx.net>
2021-10-16 09:44:06 +0200

    Update from tutorial data

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Open Collaboration Platform'
+copyright = '2021, Stefan Tröger'
+author = 'Stefan Tröger'
 
 release = '0.1'
 version = '0.1.0'
